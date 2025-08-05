25dfe9cc62960006513578fd22f92e37d298b244
Shubham Anand <143545668+shubham-anand0204@users.noreply.github.com>
2025-08-06 00:08:38 +0530

    Update AppContext.jsx
    Provided backend url

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext()
 const AppContextProvider = (props) => {
 
     const currencySymbol = '₹'
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://doc-appointment-backend-vg2w.onrender.com"
 
     const [doctors, setDoctors] = useState([])
     const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '')
